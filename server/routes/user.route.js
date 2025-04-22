f3e3e6945fd35e5b3eef9cd2aee1a1d6a8495e7b
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-04-23 01:43:31 +0530

    BE and FE connected

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,52 @@
 const express = require("express")
-
 const userRouter = express.Router()
 
+const bcrypt = require('bcrypt');
+const { userModel } = require("../models/user.model");
+var jwt = require('jsonwebtoken');
+require("dotenv").config()
+
 userRouter.post("/signup",async (req,res)=>{
+    const {firstName,lastName,email,password,dateOfBirth,city,state} = req.body
     try {
-        
+        bcrypt.hash(password, 13, async function(err, hash) {
+            if(err){
+                return res.status(400).json({msg:"Cannot register now"})
+            }
+            const newUser = await userModel.create({...req.body,password:hash})
+            return res.status(200).json({msg:"user created successfully",newUser})
+        }); 
     } catch (error) {
-        
+        return res.status(500).json({msg:"Something went wrong"})
     }
 })
 
-
+userRouter.post("/login",async (req,res)=>{
+    const {email,password} = req.body
+    try {
+        if(email && password){
+            const findUser = await userModel.findOne({email})
+            if(!findUser){
+                return res.status(404).json({msg:"User not found"})
+            }
+            bcrypt.compare(password, findUser.password, async function(err, result) {
+                if(err){
+                    return res.status(401).json({msg:"Wrong Password",err})
+                }
+                if(result){
+                    const refreshToken = jwt.sign({ userId:findUser._id }, process.env.JWT_TOKEN);
+                    findUser.refreshToken = refreshToken
+                    findUser.save()
+                    return res.status(200).json({msg:"user Logged In successfully",refreshToken})
+                }
+                return res.status(401).json({ msg: "Wrong password" });
+            });
+        }else{
+            return res.status(401).json({msg:"both email and password is required for login"})
+        } 
+    } catch (error) {
+        return res.status(500).json({msg:"Something went wrong"})
+    }
+})
 
 module.exports = {userRouter}
--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ const { connectingDB } = require("./db")
 const app = express()
 require("dotenv").config()
 app.use(express.json())
-
+const cors = require('cors');
+const { userRouter } = require("./routes/user.route")
+app.use(cors());
 
 connectingDB()
+
+app.use("/user",userRouter)
+
 
 app.use((req,res)=>{
     return res.status(404).json({msg:`Route Not Found`})
 })
 
-const port = process.env.port || 8080
+const port = process.env.port || 8000
 app.listen(port,()=>{
     console.log(`Server connected on ${port} port`)
 })
