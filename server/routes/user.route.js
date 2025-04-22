5f68e939e0e05d6dc6b25b6e7557abf8d567c079
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-04-23 00:06:55 +0530

    dont merge this one

---
+++ server/routes/user.route.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const userRouter = express.Router()
+
+userRouter.post("/signup",async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+
+
+module.exports = {userRouter}
