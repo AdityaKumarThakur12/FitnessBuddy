d51bfec2b623351cb9ce36b7ee5e3a6d064ae658
AdityaKumarThakur12 <aditya3863297@gmail.com>
2025-04-25 12:24:31 +0530

    workout done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { WorkoutProvider } from './Context/workoutContext.jsx'
+// import { WorkoutProvider } from './Context/workoutContext.jsx'
 import { AuthProvider } from './Context/AuthContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <WorkoutProvider>
+    {/* <WorkoutProvider> */}
       <App />
-    </WorkoutProvider>
+    {/* </WorkoutProvider> */}
     </AuthProvider>
   </StrictMode>,
 )
